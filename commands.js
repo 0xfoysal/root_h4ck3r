41277cf5d95863b9a7416092589efb8b6834c017
iamsolo <88438289+0xfoysal@users.noreply.github.com>
2023-07-16 11:18:48 -0700

    Add files via upload

---
+++ commands.js
@@ -0,0 +1,95 @@
+
+var twitter = "https://www.twitter.com/foysal_dev_";
+var password = "foysal";
+var linkedin = "https://www.linkedin.com/in/foysalcodeio/";
+var github = "https://github.com/0xfoysal";
+var email = 'mailto:0xfoysal@gmail.com';
+
+whois = [
+  "<br>",
+  "Hey, I'm Foysal 👋",
+  "I'm a compitive programmer, web developer & CTF player",
+  "Part time I working on web application front-end and beckend",
+  "To be honest I have no bachelor degree and most of the free time I try to learn new thing",
+  "Most of the time I take CTF compition & there are some vulnerablity I hunting like most for",
+  "example xss, rce & subdomain-takeover",
+  "<br>"
+];
+
+whoami = [
+  "<br>",
+  "In cyberSpace my name is Alex & All world is my location",
+  "<br>"
+];
+
+social = [
+  "<br>",
+  'twitter        <a href="' + twitter + '" target="_blank">foysal_dev_' + '</a>',
+  'linkedin       <a href="' + linkedin + '" target="_blank">foysalcodeio' + "</a>",
+  'github         <a href="' + github + '" target="_blank">0xfoysal' + "</a>",
+  "<br>"
+];
+
+secret = [
+  "<br>",
+  '<span class="command">sudo</span>           Only use if you\'re admin',
+  "<br>"
+];
+
+projects = [
+  "<br>",
+  "Still curating... most projects are offline, on GitHub, or confidential.",
+  "<br>"
+];
+
+help = [
+  "<br>",
+  '<span class="command">whois</span>          Who is Foysal?',
+  '<span class="command">whoami</span>         Who are you?',
+  '<span class="command">social</span>         Display social networks',
+  '<span class="command">secret</span>         Find the password',
+  '<span class="command">projects</span>       View coding projects',
+  '<span class="command">history</span>        View command history',
+  '<span class="command">help</span>           You obviously already know what this does',
+  '<span class="command">email</span>          freely message me',
+  '<span class="command">clear</span>          Clear terminal',
+  '<span class="command">banner</span>         Display the header',
+  "<br>",
+];
+
+banner = [
+  '<span class="index">.......................................................</span>',
+  "⠀⠀⠀⠀⠀⠀⠀⣀⣤⣴⣶⣶⣶⣿⣿⣿⣷⣶⣶⣶⣤⣄⠀⠀⠀⠀⠀⠀⠀⠀      ",
+"⠀⠀⠀⠀⠀⣠⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣶⣤⡀⠀⠀⠀⠀     ",
+"⠀⠀⠀⣠⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣄⠀⠀⠀     ",
+"⠀⣠⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⡀⠀     ",
+"⢰⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⡀ ",
+"⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣇ ",
+"⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿ ",
+"⣿⣿⡏⠉⠙⠿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠿⠋⠉⠉⣿⣿ ",
+"⢻⣿⡇⠀⠀⠀⠈⠙⠻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠟⠋⠀⠀⠀⠀⢀⣿⡇ ",
+"⠘⣿⣷⡀⠀⠀⠀⠀⠀⠀⠉⠛⠿⢿⣿⣿⣿⠿⠛⠋⠀⠀⠀⠀⠀⠀⢀⣼⣿⠃ ",
+"⠀⠹⣿⣿⣶⣦⣤⣀⣀⣀⣀⣀⣤⣶⠟⡿⣷⣦⣄⣀⣀⣀⣠⣤⣤⣶⣿⣿⡟⠀",
+"⠀⠀⣨⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠏⠀⡇⠸⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠃⠀",
+"⠀⢈⣿⣿⣿⣿⣿⡿⠿⠿⣿⣿⣷⠀⣼⣷⠀⣸⣿⣿⣿⡿⠿⠿⠿⣿⣿⣿⡇⠀",
+"⠀⠘⣿⣿⣿⡟⠋⠀⠀⠰⣿⣿⣿⣷⣿⣿⣷⣿⣿⣿⣿⡇⠀⠀⠀⣿⣿⠟⠁⠀",
+"⠀⠀⠈⠉⠀⠈⠁⠀⠀⠘⣿⣿⢿⣿⣿⢻⣿⡏⣻⣿⣿⠃⠀⠀⠀⠈⠀⠀⠀⠀",
+" ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣿⣿⡇⣿⣿⢸⣿⡇⣿⣿⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀",
+"⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠿⠇⢿⡿⢸⡿⠀⠿⠟⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀",
+  "      __            _                             ",
+  "    /    )        /  `                          / ",
+  "---/----/---|/--_/__-----__---------__----__---/- ",
+  "  /    /    |   /      /   ) /   / (_ ` /   ) /   ",
+  "_(____/____/|__/______(___/_(___/_(__)_(___(_/___ ",
+  "          /                    /                  ",
+  "                           (_ /                   ",
+  "_________________________________________________ ",
+  '<span class="color2">Welcome to my world.</span>',
+  "<span class=\"color2\">For a list of available commands, type</span> <span class=\"command\">'help'</span><span class=\"color2\">.</span>",
+  '<span class="index">Lets break my personal information.</span>'
+];
+
+
+
+
+
